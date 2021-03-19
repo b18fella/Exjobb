809ec9b0003c3b9fbd44eee10744b137c9a75a89
FelixLaakso <b18fella@student.his.se>
2021-03-19 15:26:22 +0100

    Initial HTML structure
    Created map for each webbapplication. One of MongoDB and one for MySQL. Also made a PHP file in each map with the initial HTML structure.

---
+++ mongodb_webbapplication/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>COVID-19 data using MongoDB</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
---
+++ mysql_webbapplication/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>COVID-19 data using MySQL</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
