565676d4f99ce0a049679f1d2a64998dbaef5e5b
FelixLaakso <b18fella@student.his.se>
2021-03-22 16:02:56 +0100

    Temporary testing Chart.js.
    Made a simple script that shows bars of random numbers as a test. Also changed the script path to the library.

--- mysql_webbapplication/index.php
+++ mysql_webbapplication/index.php
@@ -3,7 +3,7 @@
     <head>
         <title>COVID-19 data using MySQL</title>
         <link rel="stylesheet" href="main.css">
-        <script src="C:\Users\spel-\node_modules\chart.js\dist\Chart.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.min.js"></script>
     </head>
     <body>
         <header>
@@ -22,4 +22,38 @@
         <section id="cc">
         </section>
     </body>
+
+    <script>
+        var canvas = document.getElementById('covidChart');
+        var covidChart = new Chart(canvas, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Black', 'Yellow'],
+                datasets: [{
+                    label: 'number of cases',
+                    data: [15, 20, 31], //The data goes here.
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
 </html>
