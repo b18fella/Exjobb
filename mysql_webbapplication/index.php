609ce530ccef5fb5f5c2550b35b8ec48f1f9ca6b
FelixLaakso <b18fella@student.his.se>
2021-03-22 12:35:50 +0100

    Header creation
    Creating the beginning of a header and also adding a CSS file.

--- mysql_webbapplication/index.php
+++ mysql_webbapplication/index.php
@@ -4,6 +4,15 @@
         <title>COVID-19 data using MySQL</title>
     </head>
     <body>
-        
+        <header>
+            <div id="logo">MySQL</div>
+            <div>
+                <ul>
+                    <li>COVID-19 Data</li>
+                    <li>Info</li>
+                    <li>CC</li>
+                </ul>
+            </div>
+        </header>
     </body>
 </html>
