43afa3e9a4376f115963db9cead7904aae204876
FelixLaakso <b18fella@student.his.se>
2021-03-24 16:22:58 +0100

    Still testing and changing stuff around.

--- mysql_webbapplication/index.php
+++ mysql_webbapplication/index.php
@@ -40,5 +40,37 @@
         </section>
     </body>
 
-    <script src="charts.js"></script>
+    <script>
+    var canvas = document.getElementById('covidChart');
+    var covidChart = new Chart(canvas, {
+        type: 'bar', //Type of chart, in this case, bar chart.
+        data: {
+            labels: ['Red', 'Black', 'Yellow'],
+            datasets: [{
+                label: 'number of cases', //Label on top of the chart.
+                data: [15, 20, 1111], //The data goes here.
+                backgroundColor: [ //Color of each bar, left to right.
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(0, 0, 0, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [ //Border color of each bar, left to right.
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(0, 0, 0, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    </script>
 </html>
--- mysql_webbapplication/selections.php
+++ mysql_webbapplication/selections.php
@@ -2,12 +2,12 @@
     include 'databaseConnection.php';
 
     if (isset($_POST['WHO_region'])) {
-        $sqlQuery = "SELECT Cumulative_cases FROM globalcoviddata WHERE country =" . $_POST['WHO_region'];
+        $sqlQuery = "SELECT Cumulative_cases FROM globalcoviddata WHERE WHO_region =" . $_POST['WHO_region'];
 
         $queryResult = $databaseConnection->query($sqlQuery);
 
         while ($row = $queryResult->fetch_assoc()) {
-            
+
         }
     }
 ?>
