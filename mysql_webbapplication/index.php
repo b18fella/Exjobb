c7f1ac5b8fd469327121aaba661c4000713104e1
FelixLaakso <b18fella@student.his.se>
2021-04-19 00:44:04 +0200

    Changed the JS source to the correct filename.

--- mysql_webbapplication/index.php
+++ mysql_webbapplication/index.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" href="../main.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="../data.js"></script>
+        <script src="../dataHandling.js"></script>
         <script>
                 $(document).ready(function() {
                     $("select").on('change', function() {
