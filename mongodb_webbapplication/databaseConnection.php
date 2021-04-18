59c7b8f59f6783c2c1243e33ffe11840c3e627c2
FelixLaakso <b18fella@student.his.se>
2021-04-19 00:47:30 +0200

    Adding MongoDB queries.
    Adding two different queries for all regions and specific region. Also executing the query, converting to array and sending it back to the ajax code.

--- mongodb_webbapplication/databaseConnection.php
+++ mongodb_webbapplication/databaseConnection.php
@@ -1,3 +1,15 @@
 <?php
     $databaseConnection = new MongoDB\Driver\Manager();
+
+    if ($_GET['query'] == 'ALL') {
+        $mongoCommand = new MongoDB\Driver\Query([]);
+    } else {
+        $mongoCommand = new MongoDB\Driver\Query(['WHO_region' => $_GET['query']]);
+    }
+
+    $queryResult = $databaseConnection->executeQuery('coviddata.globalcoviddata', $mongoCommand);
+    $queryResult = $queryResult->toArray();
+
+
+    echo json_encode($queryResult);
 ?>
