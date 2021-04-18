7013b9c78c66ea94c6cd83d30e2240db839af7ce
FelixLaakso <b18fella@student.his.se>
2021-04-19 01:27:56 +0200

    Adding the correct sorting.

--- mongodb_webbapplication/databaseConnection.php
+++ mongodb_webbapplication/databaseConnection.php
@@ -2,14 +2,13 @@
     $databaseConnection = new MongoDB\Driver\Manager();
 
     if ($_GET['query'] == 'ALL') {
-        $mongoQuery = new MongoDB\Driver\Query([]);
+        $mongoQuery = new MongoDB\Driver\Query([], ['sort' => ['Date_reported' => 1]]);
     } else {
-        $mongoQuery = new MongoDB\Driver\Query(['WHO_region' => $_GET['query']], ['sort' => ['Date_reported' => 1]]);
+    $mongoQuery = new MongoDB\Driver\Query(['WHO_region' => $_GET['query']], ['sort' => ['Date_reported' => 1]]);
     }
 
     $queryResult = $databaseConnection->executeQuery('coviddata.globalcoviddata', $mongoQuery);
     $queryResult = $queryResult->toArray();
-
 
     echo json_encode($queryResult);
 ?>
