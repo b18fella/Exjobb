2fccbf2cf312f5bdeb9ac2691354455ccb363d75
FelixLaakso <b18fella@student.his.se>
2021-04-19 00:43:13 +0200

    Adding jQuery and JS sources

--- mongodb_webbapplication/index.php
+++ mongodb_webbapplication/index.php
@@ -4,7 +4,26 @@
         <title>COVID-19 data using MongoDB</title>
         <link rel="stylesheet" href="../main.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.min.js"></script>
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="../dataHandling.js"></script>
+        <script>
+                $(document).ready(function() {
+                    $("select").on('change', function() {
+                        $.ajax({
+                            url: 'databaseConnection.php?query=' + this.value,
+                            type: 'get',
+                            dataType: 'json',
+                            success: function(data) {
+                                console.log(data);
+                                drawGraph(formatData(data));
+                            },
+                            error: function(request, status, error) {
+                                console.error(error);
+                            }
+                        });
+                    });
+                });
+        </script>
     </head>
     <body>
         <header>
@@ -26,7 +45,6 @@
                     <option value="SEARO" name='WHO_region'>South-East Asia Region</option>
                     <option value="ALL" name='WHO_region'>All regions</option>
                 </select>
-                <input type="submit" name="submit" value="Submit">
             </form>
             <div id="covidDataContainer">
                 <canvas id="covidChart"></canvas>
@@ -37,7 +55,7 @@
         <section id="cc">
         </section>
     </body>
-    <script type='text/javascript'>
+    <!--<script type='text/javascript'>
         var canvas = document.getElementById('covidChart');
         var covidChart = new Chart(canvas, {
             type: 'line', //Type of chart, in this case, bar chart.
@@ -58,5 +76,5 @@
                 }
             }
         });
-    </script>
+    </script>-->
 </html>
