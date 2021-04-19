f987e7214f56b4ddb853b7f6390644a5a8824848
FelixLaakso <b18fella@student.his.se>
2021-04-19 19:53:58 +0200

    Solving the problem on the MySQL side.

--- mysql_webbapplication/databaseConnection.php
+++ mysql_webbapplication/databaseConnection.php
@@ -13,45 +13,18 @@
     
     $WHO_region = $_GET['query'];
     if($WHO_region == 'ALL') {
-        $sqlQuery = "SELECT * FROM globalcoviddata;";
+        $sqlQuery = "SELECT * FROM globalcoviddata ORDER BY Date_reported, Country;";
     } else {
-        $sqlQuery = "SELECT * FROM globalcoviddata WHERE WHO_region = '$WHO_region';";
+        $sqlQuery = "SELECT * FROM globalcoviddata WHERE WHO_region = '$WHO_region' ORDER BY Date_reported, Country;";
     }
     
     $queryResult = $databaseConnection->query($sqlQuery);
 
     if (mysqli_num_rows($queryResult) > 0) {
         $resultArray = array();
-        $resultArray['Regions'] = array(
-            "EMRO" => array(),
-            "EURO" => array(),
-            "AFRO" => array(),
-            "WPRO" => array(),
-            "AMRO" => array(),
-            "SEARO" => array(),
-            "Other" => array()
-        );
-        $country = '';
-        $firstCountry = true;
         while ($row = $queryResult->fetch_assoc()) {
-            if ($country !== $row['Country']) {
-                if ($firstCountry && $country === '') {
-                    $resultArray['Date_reported'] = array();
-                } else {
-                    $firstCountry = false;
-                }
-                $resultArray['Regions'][$row['WHO_region']][$row['Country']] = array();
-                $country = $row['Country'];
-            }
-            if ($firstCountry) {
-                $resultArray['Date_reported'][] = $row['Date_reported'];
-            }
-            $resultArray['Regions'][$row['WHO_region']][$row['Country']][] = array(
-                'Cumulative_cases' => $row['Cumulative_cases'],
-                'Cumulative_deaths' => $row['Cumulative_deaths']
-            );
+            $resultArray[] = $row;
         }
-
         echo json_encode($resultArray);
     } else {
         echo "These was no data";
