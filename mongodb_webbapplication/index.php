c1870b7606dbbb0a92605106f27a332e098683d7
FelixLaakso <b18fella@student.his.se>
2021-04-19 19:15:31 +0200

    Removing the jQuery code from index.php files.

--- mongodb_webbapplication/index.php
+++ mongodb_webbapplication/index.php
@@ -6,23 +6,6 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="../dataHandling.js"></script>
-        <script>
-                $(document).ready(function() {
-                    $("select").on('change', function() {
-                        $.ajax({
-                            url: 'databaseConnection.php?query=' + this.value,
-                            type: 'get',
-                            dataType: 'json',
-                            success: function(data) {
-                                drawGraph(formatData(data));
-                            },
-                            error: function(request, status, error) {
-                                console.error(error);
-                            }
-                        });
-                    });
-                });
-        </script>
     </head>
     <body>
         <header>
