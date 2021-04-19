4a74bafd18adecc6f247e76453b0f4bf3bd314f5
FelixLaakso <b18fella@student.his.se>
2021-04-19 06:35:50 +0200

    Adding code so that the data gets inserted in the correct place.

--- mongodb_webbapplication/databaseConnection.php
+++ mongodb_webbapplication/databaseConnection.php
@@ -2,13 +2,44 @@
     $databaseConnection = new MongoDB\Driver\Manager();
 
     if ($_GET['query'] == 'ALL') {
-        $mongoQuery = new MongoDB\Driver\Query([], ['sort' => ['Date_reported' => 1]]);
+        $mongoQuery = new MongoDB\Driver\Query([]);
     } else {
-    $mongoQuery = new MongoDB\Driver\Query(['WHO_region' => $_GET['query']], ['sort' => ['Date_reported' => 1]]);
+    $mongoQuery = new MongoDB\Driver\Query(['WHO_region' => $_GET['query']]);
     }
 
     $queryResult = $databaseConnection->executeQuery('coviddata.globalcoviddata', $mongoQuery);
     $queryResult = $queryResult->toArray();
 
-    echo json_encode($queryResult);
+        $resultArray = array();
+        $resultArray['Regions'] = array(
+            "EMRO" => array(),
+            "EURO" => array(),
+            "AFRO" => array(),
+            "WPRO" => array(),
+            "AMRO" => array(),
+            "SEARO" => array(),
+            "Other" => array()
+        );
+        $country = '';
+        $firstCountry = true;
+        foreach ($queryResult as $row) {
+            if ($country !== $row->Country) {
+                if ($firstCountry && $country === '') {
+                    $resultArray['Date_reported'] = array();
+                } else {
+                    $firstCountry = false;
+                }
+                $resultArray['Regions'][$row->WHO_region][$row->Country] = array();
+                $country = $row->Country;
+            }
+            if ($firstCountry) {
+                $resultArray['Date_reported'][] = $row->Date_reported;
+            }
+            $resultArray['Regions'][$row->WHO_region][$row->Country][] = array(
+                'Cumulative_cases' => $row->Cumulative_cases,
+                'Cumulative_deaths' => $row->Cumulative_deaths
+            );
+        }
+
+        echo json_encode($resultArray);
 ?>
